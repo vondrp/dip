6446d3f76c1ba88fe8b3df1d5ffbb27a589214ca
vondrp <vondrovic@centrum.cz>
2025-04-10 13:41:50 +0200

    název složky logs upraven na output

--- profiler_tool/config/paths.py
+++ profiler_tool/config/paths.py
@@ -3,11 +3,11 @@ import os
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 BUILD_DIR = os.path.join(BASE_DIR, "build")
-LOGS_DIR = os.path.join(BASE_DIR, "logs")
-TRACE_DIR = os.path.join(LOGS_DIR, "traces")
-KLEE_OUTPUT = os.path.join(LOGS_DIR, "klee-output")
-KLEE_RESULTS = os.path.join(LOGS_DIR, "klee-results")
-ANALYSIS_DIR = os.path.join(LOGS_DIR, "analysis")
+OUTPUT_DIR = os.path.join(BASE_DIR, "output")
+TRACE_DIR = os.path.join(OUTPUT_DIR, "traces")
+KLEE_OUTPUT = os.path.join(OUTPUT_DIR, "klee-output")
+KLEE_RESULTS = os.path.join(OUTPUT_DIR, "klee-results")
+ANALYSIS_DIR = os.path.join(OUTPUT_DIR, "analysis")
 
 DEFAULT_GENERATED_MAIN = os.path.join(BASE_DIR, "tests", "src", "generated_main.c")
 DEFAULT_GENERATED_MAIN_KLEE = os.path.join(BASE_DIR, "tests", "src", "generated_main_klee.c")
